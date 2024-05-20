1578ade3529f87bba535e8704f833a717caa24e7
Emanuel Quintino <emanuelquintino@hotmail.com>
2024-05-20 16:27:13 -0300

    update return types app error

--- src/errors/appError.ts
+++ src/errors/appError.ts
@@ -1,4 +1,4 @@
-export function appError(message: string, status: number): Error & { status: number } {
+export function appError(message: string, status: number) {
   const error = new Error(message) as Error & { status: number };
   error.status = status;
   return error;
