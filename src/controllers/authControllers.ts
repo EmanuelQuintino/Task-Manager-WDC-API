c640e0e58291106dabbd78f2665fd3f4d44fb257
Emanuel Quintino <emanuelquintino@hotmail.com>
2024-05-29 22:28:19 -0300

    destructure auth token and id

--- src/controllers/authControllers.ts
+++ src/controllers/authControllers.ts
@@ -8,16 +8,16 @@ export const authControllers = {
     try {
       const { email, password } = loginSchema.parse(req.body);
 
-      const userAuth = await authServices.login({ email, password }, userRepository);
+      const { token, id } = await authServices.login({ email, password }, userRepository);
 
-      res.cookie(process.env.KEY_TOKEN, userAuth.token, {
+      res.cookie(process.env.KEY_TOKEN, token, {
         httpOnly: true,
         sameSite: "none",
         secure: true,
         maxAge: 1000 * 60 * 60 * 18, // 18h
       });
 
-      return res.status(200).json({ message: "login successful!", id: userAuth.id });
+      return res.status(200).json({ message: "login successful!", id });
     } catch (error) {
       return next(error);
     }
